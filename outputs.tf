5485ecdcd05d2ce91e692456f3ba62403a5e8845
crycrime <crycrime@gmail.com>
2021-02-21 10:48:25 +0200

    init

--- outputs.tf
+++ outputs.tf
@@ -5,8 +5,3 @@ output "eks_endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = data.aws_eks_cluster.cluster_adv.certificate_authority[0].data
 }
-
-output "ec2_endpoint" {
-  description = "The address of the EC2 instance"
-  value       = aws_instance.ec2-instance.public_ip
-}
